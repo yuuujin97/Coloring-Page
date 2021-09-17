1efe089e34114ed4123f3cc74c5d3398ed121a86
Kim YuJin <yuuujin5468@gmail.com>
2021-09-17 19:31:38 +0900

    Added header and text coloring function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const drag_elements = document.querySelectorAll('.drag_element');
-const sections = document.querySelectorAll('.mockup_section');
+const areas = document.querySelectorAll('.paint_area');
+const texts = document.querySelectorAll('.paint_text');
 let color;
 
 for (const drag_element of drag_elements) {
@@ -10,11 +11,18 @@ for (const drag_element of drag_elements) {
   drag_element.addEventListener('dragend', dragEnd);
 }
 
-for (const section of sections) {
-  section.addEventListener('dragover', dragOver);
-  section.addEventListener('dragenter', dragEnter);
-  section.addEventListener('dragleave', dragLeave);
-  section.addEventListener('drop', dragDrop);
+for (const area of areas) {
+  area.addEventListener('dragover', dragOver);
+  area.addEventListener('dragenter', dragEnter);
+  area.addEventListener('dragleave', dragLeave);
+  area.addEventListener('drop', dragDrop);
+}
+
+for (const text of texts) {
+  text.addEventListener('dragover', dragOver);
+  text.addEventListener('dragenter', dragEnter);
+  text.addEventListener('dragleave', dragLeave);
+  text.addEventListener('drop', dragDrop);
 }
 
 function dragStart() {
@@ -27,11 +35,21 @@ function dragEnd() {
 
 function dragOver(e) {
   e.preventDefault();
+  e.stopPropagation();
+  this.classList.add('hold');
 }
 function dragEnter(e) {
   e.preventDefault();
 }
-function dragLeave() {}
-function dragDrop() {
-  this.style.backgroundColor = color;
+function dragLeave() {
+  this.classList.remove('hold');
+}
+function dragDrop(e) {
+  e.stopPropagation();
+  this.classList.remove('hold');
+  if (this.classList.contains('paint_text')) {
+    this.style.color = color;
+  } else if (this.classList.contains('paint_area')) {
+    this.style.backgroundColor = color;
+  }
 }
