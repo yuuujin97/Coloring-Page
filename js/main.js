2052611e0b67c0c13f7692b68245a1abb5f5bb2c
Kim YuJin <yuuujin5468@gmail.com>
2021-09-25 20:52:01 +0900

    Edited code for you can drag and drop in mobile

--- js/main.js
+++ js/main.js
@@ -1,64 +1,73 @@
 'use strict';
 
+const drag_color = document.querySelector('.input_color');
 const areas = document.querySelectorAll('.paint_area');
 const texts = document.querySelectorAll('.paint_text');
-const drag = document.querySelector('.input_color');
 const reset_button = document.querySelector('.reset_button');
+const position = { x: 0, y: 0 };
 let color;
 
-drag.addEventListener('dragstart', dragStart);
-drag.addEventListener('dragend', dragEnd);
-
-for (const area of areas) {
-  area.addEventListener('dragover', dragOver);
-  area.addEventListener('dragenter', dragEnter);
-  area.addEventListener('dragleave', dragLeave);
-  area.addEventListener('drop', dragDrop);
-}
-
-for (const text of texts) {
-  text.addEventListener('dragover', dragOver);
-  text.addEventListener('dragenter', dragEnter);
-  text.addEventListener('dragleave', dragLeave);
-  text.addEventListener('drop', dragDrop);
-}
-
-reset_button.addEventListener('click', colorReset);
-
-function dragStart() {
-  color = this.value;
-}
-
-function dragEnd() {
-  color = this.value;
-}
-
-function dragOver(e) {
-  e.preventDefault();
-  e.stopPropagation();
-  this.classList.add('hold');
-}
-function dragEnter(e) {
-  e.preventDefault();
-}
-function dragLeave() {
-  this.classList.remove('hold');
-}
-function dragDrop(e) {
-  e.stopPropagation();
-  this.classList.remove('hold');
-  if (this.classList.contains('paint_text')) {
-    this.style.color = color;
-  } else if (this.classList.contains('paint_area')) {
-    this.style.backgroundColor = color;
-  }
-}
-
-function colorReset() {
+reset_button.addEventListener('click', () => {
   for (const area of areas) {
     area.style.backgroundColor = '';
   }
   for (const text of texts) {
     text.style.color = '';
   }
+});
+
+function addDropzone(elements) {
+  for (let type of elements) {
+    interact(type).dropzone({
+      accept: '#drag_drop',
+      overlap: 0.5,
+
+      ondragenter: function (event) {
+        let dropzoneElement = event.target;
+        dropzoneElement.classList.add('hold');
+      },
+
+      ondragleave: function (event) {
+        event.target.classList.remove('hold');
+      },
+
+      ondrop: function (event) {
+        event.target.classList.remove('hold');
+        if (event.target.classList.contains('paint_text')) {
+          event.target.style.color = color;
+        } else if (event.target.classList.contains('paint_area')) {
+          event.target.style.backgroundColor = color;
+        }
+      },
+    });
+  }
 }
+
+addDropzone(areas);
+addDropzone(texts);
+
+interact(drag_color)
+  .on('tap', () => {
+    drag_color.removeAttribute('disabled');
+  })
+  .draggable({
+    listeners: {
+      start(event) {
+        color = event.target.value;
+      },
+
+      move(event) {
+        position.x += event.dx;
+        position.y += event.dy;
+
+        event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
+      },
+
+      end(event) {
+        position.x = 0;
+        position.y = 0;
+        event.target.style.transform = `translate(0, 0)`;
+        drag_color.setAttribute('disabled', 'true');
+      },
+    },
+  });
